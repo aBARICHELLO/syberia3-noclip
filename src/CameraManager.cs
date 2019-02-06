c889ccc63114ffade122b877f21e1b29d98b25ce
BARICHELLO <artur@barichello.me>
2019-02-06 20:38:32 -0200

    Implement camera translation

--- src/CameraManager.cs
+++ src/CameraManager.cs
@@ -24,38 +24,51 @@ public class CameraManager : BaseKoaUScriptEvent, IBackupHandler {
     }
 
     public void CheatCamera() {
-        float speedModifier = 1f;
-        if (Input.GetKey(KeyCode.LeftShift)) {
-            speedModifier = 7f;
-        }
-
-        // Camera Translation
-        this.m_mainCamera.transform.position = this.lastFramePosition;
-        if (Input.GetKey(KeyCode.PageUp)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.right;
-        }
-        if (Input.GetKey(KeyCode.PageDown)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.Home)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.up;
-        }
-        if (Input.GetKey(KeyCode.End)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.down;
-        }
-        if (Input.GetKey(KeyCode.Insert)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.Delete)) {
-            this.m_mainCamera.transform.position += Time.deltaTime * speedModifier * Vector3.back;
-        }
-        this.lastFramePosition = this.m_mainCamera.transform.position;
+        Camera camera = this.m_mainCamera;
 
         // Camera Rotation
         this.cameraXAngle += cameraSpeed * Input.GetAxis("Mouse X");
         this.cameraYAngle -= cameraSpeed * Input.GetAxis("Mouse Y");
         Mathf.Clamp(this.cameraYAngle, minCameraAngle, maxCameraAngle);
-        this.m_mainCamera.transform.eulerAngles = new Vector3(this.cameraYAngle, this.cameraXAngle, 0f);
+        camera.transform.eulerAngles = new Vector3(this.cameraYAngle, this.cameraXAngle, 0f);
+
+        // Camera Translation
+        camera.transform.position = this.lastFramePosition;
+        float speedModifier = 1f;
+        if (Input.GetKey(KeyCode.H)) { // boost
+            speedModifier = 7f;
+        }
+        if (Input.GetKey(KeyCode.I)) { // forwards
+            Vector3 direction = camera.transform.forward;
+            direction.Normalize();
+            camera.transform.position += Time.deltaTime * speedModifier * direction;
+        }
+        if (Input.GetKey(KeyCode.K)) { // backwards
+            Vector3 direction = camera.transform.forward;
+            direction.Normalize();
+            camera.transform.position -= Time.deltaTime * speedModifier * direction;
+        }
+        if (Input.GetKey(KeyCode.J)) { // left
+            Vector3 direction = camera.transform.right;
+            direction.Normalize();
+            camera.transform.position -= Time.deltaTime * speedModifier * direction;
+        }
+        if (Input.GetKey(KeyCode.L)) { // right
+            Vector3 direction = camera.transform.right;
+            direction.Normalize();
+            camera.transform.position += Time.deltaTime * speedModifier * direction;
+        }
+        if (Input.GetKey(KeyCode.U)) { // down
+            Vector3 direction = camera.transform.up;
+            direction.Normalize();
+            camera.transform.position -= Time.deltaTime * speedModifier * direction;
+        }
+        if (Input.GetKey(KeyCode.O)) { // up
+            Vector3 direction = camera.transform.up;
+            direction.Normalize();
+            camera.transform.position += Time.deltaTime * speedModifier * direction;
+        }
+        this.lastFramePosition = camera.transform.position;
     }
 
     [HideInInspector]
